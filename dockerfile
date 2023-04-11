94534c4021214477354b8c26120d5c2e246cdaf2
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-04-11 09:19:20 -0600

    update on dev

--- dockerfile
+++
@@ -1,4 +0,0 @@
-FROM node:18-buster
-
-WORKDIR /code
-
--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Employee from "./Compoents/Employee";
-import { useState } from 'react';
 
 function App() {
-    const [role, setRole] = useState('Intern');
-    const showEmployees = true;
   return (
     <div className="App">
-
-      <h1 className="text-3xl font-bold underline">Hello React</h1>
       <header className="App-header">
-
         <img src={logo} className="App-logo" alt="logo" />
-          {showEmployees ? (
-              <>
-              <input type='text' onChange={(e) => {
-                console.log(e.target.value);
-                setRole(e.target.value);
-              }}/>
-              <div className=''>
-                  <Employee name = 'Taiko' role = 'Software Engineer, Dev-Ops and Cyper Sec'/>
-                  <Employee  name = 'Abby' role = {role}/>
-                  <Employee name = 'Ivara'/>
-                  <Employee name = 'Leon' />
-              </div>
-                
-              </>
-
-              ) : (
-                  <p> You cannot see the employees</p>
-          )
-          }
-
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
