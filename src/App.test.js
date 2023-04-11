224cc6e51e9e925bd0b207377699251a23137b2b
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-04-11 09:09:24 -0600

    setup of project and getting ready for docker
    Basic setup of the card demo project using create react app node v19.8.1, and styling using Tailwind CSS and Daisy UI libraries. Testing with jest and Cypress, and CI/CD with Docker.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('finds Hello React', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello React/i);
   expect(linkElement).toBeInTheDocument();
 });
