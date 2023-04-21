d6dadd338264f8848bbb27d93a92bbe7234da2ff
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-04-20 21:49:58 -0600

    new setup of card demo

--- src/compoents/Employee.js
+++ src/compoents/Employee.js
@@ -1,7 +1,20 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee ()  {
     return(
-        <div>
+        <div className="card w-96 glass py-8 px-8 max-w-sm mx-auto space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <div className="card w-96 glass shadow-lg">
+                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Life hack</h2>
+                    <p>How to park your car at your garage?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Learn now!</button>
 
+                    </div>
+                    <EditEmployee />
+                </div>
+            </div>
         </div>
     )
 }
