88f89882cf7f4c84e0991c15f8b2e97d43f9e6be
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-04-23 22:12:15 -0600

    Update Employee.js
    added better formatting of the main employee card. In prep for data fetching and updating the modal

--- src/compoents/Employee.js
+++ src/compoents/Employee.js
@@ -1,21 +1,21 @@
 import EditEmployee from "./EditEmployee";
 
+
 function Employee ()  {
     return(
-        <div className="card w-96 glass py-8 px-8 max-w-sm mx-auto space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <div className="card w-96 glass shadow-lg">
+
+            <div className="card w-96 glass shadow-lg py-8 px-8 max-w-sm mx-auto space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
                 <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
                 <div className="card-body">
                     <h2 className="card-title">Life hack</h2>
                     <p>How to park your car at your garage?</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Learn now!</button>
-
+                        <EditEmployee />
                     </div>
-                    <EditEmployee />
                 </div>
             </div>
-        </div>
+
     )
 }
 export default Employee;
